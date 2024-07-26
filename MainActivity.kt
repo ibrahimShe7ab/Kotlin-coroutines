6ea5e815a1a93c7ea0546a3f67681054e45bd7e4
ibrahimShehab <136454516+ibrahimShe7ab@users.noreply.github.com>
2024-07-26 11:09:14 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -14,112 +14,112 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        // Example of starting a new thread
-//        val thread = object : Thread() {
-//            override fun run() {
-//                super.run()
-//                Log.d(TAG, "Thread implementation")
-//            }
-//        }
-//        thread.start()
-//
-//        // Example of using GlobalScope to launch a coroutine
-//        GlobalScope.launch {
-//            delay(1000L)
-//            Log.d(TAG, "implementation coroutines ${Thread.currentThread().name}")
-//        }
-//
-//        // Using different dispatchers with lifecycleScope
-//        lifecycleScope.launch(Dispatchers.Unconfined) {
-//            delay(1000L)
-//            Log.d(TAG, "implementation Unconfined coroutines ${Thread.currentThread().name}")
-//        }
-//
-//        lifecycleScope.launch(Dispatchers.Main) {
-//            delay(1000L)
-//            Log.d(TAG, "implementation Main coroutines ${Thread.currentThread().name}")
-//        }
-//
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            delay(1000L)
-//            Log.d(TAG, "implementation IO coroutines ${Thread.currentThread().name}")
-//        }
-//
-//        lifecycleScope.launch(Dispatchers.Default) {
-//            delay(1000L)
-//            Log.d(TAG, "implementation Default coroutines ${Thread.currentThread().name}")
-//        }
-//
-//        // Example of nested coroutines
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            Log.d(TAG, "implementation IO coroutines ${Thread.currentThread().name}")
-//
-//            withContext(Dispatchers.Main) {
-//                delay(1000L)
-//                Log.d(TAG, "implementation Main coroutines ${Thread.currentThread().name}")
-//            }
-//            withContext(Dispatchers.Default) {
-//                delay(1000L)
-//                Log.d(TAG, "implementation Default coroutines ${Thread.currentThread().name}")
-//            }
-//        }
-//
-//        // Example of a suspend function
-//        suspend fun loadImage(): String {
-//            return "done!"
-//        }
-//
-//        // Using runBlocking to bridge the non-coroutine world to the coroutine world
-//        runBlocking {
-//            launch(Dispatchers.Main) {
-//                val result = loadImage()
-//                Log.d(TAG, result)
-//
-//                launch(Dispatchers.IO) {
-//                    Log.d(TAG, "Run Block IO${Thread.currentThread().name}")
-//                }
-//            }
-//        }
-//
-//        // Example of using Job to manage coroutine lifecycle
-//        val myJob: Job = GlobalScope.launch(Dispatchers.Default) {
-//            if (isActive) {
-//                repeat(5) {
-//                    Log.d(TAG, "JOB still working")
-//                    delay(2000L)
-//                }
-//            }
-//        }
-//
-//        // Run a blocking coroutine
-//        runBlocking {
-//            Log.d(TAG, "start coroutine")
-//            myJob.join() // Wait until myJob completes
-//            myJob.cancel() // Cancel this job
-//            Log.d(TAG, "finishing coroutine")
-//        }
-//
-//        // Example of measuring execution time of coroutines
-//        suspend fun num1(): Int {
-//            return 10
-//        }
-//
-//        suspend fun num2(): Int {
-//            return 10
-//        }
-//
-//        runBlocking {
-//            val time = measureTimeMillis {
-//                val num1Deferred = async { num1() }
-//                val num2Deferred = async { num2() }
-//
-//                if (num1Deferred.await() == num2Deferred.await()) {
-//                    Log.d(TAG, "equal")
-//                }
-//            }
-//            Log.d(TAG, "time response $time Millis")
-//        }
+
+       // Example of starting a new thread
+       val thread = object : Thread() {
+           override fun run() {
+               super.run()
+               Log.d(TAG, "Thread implementation")
+           }
+       }
+       thread.start()
+
+       // Example of using GlobalScope to launch a coroutine
+       GlobalScope.launch {
+           delay(1000L)
+           Log.d(TAG, "implementation coroutines ${Thread.currentThread().name}")
+       }
+
+       // Using different dispatchers with lifecycleScope
+       lifecycleScope.launch(Dispatchers.Unconfined) {
+           delay(1000L)
+           Log.d(TAG, "implementation Unconfined coroutines ${Thread.currentThread().name}")
+       }
+
+       lifecycleScope.launch(Dispatchers.Main) {
+           delay(1000L)
+           Log.d(TAG, "implementation Main coroutines ${Thread.currentThread().name}")
+       }
+
+       lifecycleScope.launch(Dispatchers.IO) {
+           delay(1000L)
+           Log.d(TAG, "implementation IO coroutines ${Thread.currentThread().name}")
+       }
+
+       lifecycleScope.launch(Dispatchers.Default) {
+           delay(1000L)
+           Log.d(TAG, "implementation Default coroutines ${Thread.currentThread().name}")
+       }
+
+       // Example of nested coroutines
+       lifecycleScope.launch(Dispatchers.IO) {
+           Log.d(TAG, "implementation IO coroutines ${Thread.currentThread().name}")
+
+           withContext(Dispatchers.Main) {
+               delay(1000L)
+               Log.d(TAG, "implementation Main coroutines ${Thread.currentThread().name}")
+           }
+           withContext(Dispatchers.Default) {
+               delay(1000L)
+               Log.d(TAG, "implementation Default coroutines ${Thread.currentThread().name}")
+           }
+       }
+
+       // Example of a suspend function
+       suspend fun loadImage(): String {
+           return "done!"
+       }
+
+       // Using runBlocking to bridge the non-coroutine world to the coroutine world
+       runBlocking {
+           launch(Dispatchers.Main) {
+               val result = loadImage()
+               Log.d(TAG, result)
+
+               launch(Dispatchers.IO) {
+                   Log.d(TAG, "Run Block IO${Thread.currentThread().name}")
+               }
+           }
+       }
+
+       // Example of using Job to manage coroutine lifecycle
+       val myJob: Job = GlobalScope.launch(Dispatchers.Default) {
+           if (isActive) {
+               repeat(5) {
+                   Log.d(TAG, "JOB still working")
+                   delay(2000L)
+               }
+           }
+       }
+
+       // Run a blocking coroutine
+       runBlocking {
+           Log.d(TAG, "start coroutine")
+           myJob.join() // Wait until myJob completes
+           myJob.cancel() // Cancel this job
+           Log.d(TAG, "finishing coroutine")
+       }
+
+       // Example of measuring execution time of coroutines
+       suspend fun num1(): Int {
+           return 10
+       }
+
+       suspend fun num2(): Int {
+           return 10
+       }
+
+       runBlocking {
+           val time = measureTimeMillis {
+               val num1Deferred = async { num1() }
+               val num2Deferred = async { num2() }
+
+               if (num1Deferred.await() == num2Deferred.await()) {
+                   Log.d(TAG, "equal")
+               }
+           }
+           Log.d(TAG, "time response $time Millis")
+       }
 
 
 
